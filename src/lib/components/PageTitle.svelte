8b343e3fc5ced38b04db1c33d3970fdfc221f541
Горбулёв Сергей <gorbulevsv@yandex.ru>
2022-12-29 19:10:19 +0300

    Переделан PageTitle

--- src/lib/components/PageTitle.svelte
+++ src/lib/components/PageTitle.svelte
@@ -2,7 +2,9 @@
 	export let title = '';
 </script>
 
-<div class="d-flex justify-content-between align-items-center bg-dark text-light p-3 rounded">
-	<h5>{title}</h5>
-	<slot />
+<div class="d-flex justify-content-between align-items-center bg-dark text-light rounded p-3 mb-3">
+	<div>
+		<h5 class="mb-3">{title}</h5>
+		<slot />
+	</div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,8 @@
 	import PageTitle from '$lib/components/PageTitle.svelte';
 </script>
 
-<PageTitle title="Всем привет!" />
+<PageTitle title="Всем привет!">
+	<div class="bg-light text-dark p-2 rounded">
+		<div>Как вы поживаете?</div>
+	</div>
+</PageTitle>
